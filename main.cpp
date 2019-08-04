0ba596e230899d70b6260e5735b60dba531e5de0
mezorian <mezorian@inc-ffh.solutions>
2019-08-04 21:01:18 +0200

    initially added sources

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include "ModuleA.h"
+#include "ModuleB.h"
+#include "ModuleC.h"
+
+int main() {
+    ModuleA a;
+    ModuleB b;
+    ModuleC c;
+    a.doSmth();
+    b.doSmth();
+    c.doSmth();
+    return 0;
+}
